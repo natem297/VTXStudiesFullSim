6fe5c71be93b70db5554bdedfcde6d33adc87e3d
natem297 <mnate5002@gmail.com>
2024-07-26 10:51:11 +0200

    added initial file

---
+++ python/higgs_mumu_tester.py
@@ -0,0 +1,4 @@
+from podio import root_io
+
+input_file_path = "/eos/experiment/fcc/prod/fcc/ee/test_spring2024/240gev/mumuH/CLD_o2_v05/rec/00016478/000/"
+podio_reader = root_io.Reader(input_file_path)
