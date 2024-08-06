cbd41bb457accdcd5084ad8d507291a4e7245e76
natem297 <mnate5002@gmail.com>
2024-08-06 14:50:30 +0200

    occupancy calculation now based on modules

--- python/occupancy.py
+++ python/occupancy.py
@@ -8,9 +8,6 @@ podio_reader = root_io.Reader(input_file_path)
 events = podio_reader.get("events")
 
 layer_radii = [14, 23, 34.5, 141, 316]
-true_radii = [13.7, 23.7, 34]
-disk_z = [303, 635, 945, -303, -635, -945]
-pixel_size = 0.025
 
 def phi(x,y):
     """
@@ -37,57 +34,19 @@ def radius(hit):
             return r
     raise ValueError(f"Not close enough to any of the layers {np.sqrt(true_radius)}")
 
-def z_coord(hit):
-    """
-    Calculates z coordinate of hit.
-    Inputs: hit, SimTrackerHit object.
-    Output: z, int representing z coordinate of hit in mm.
-    """
-    true_z = hit.getPosition().z
-    for z in disk_z:
-        if abs(true_z-z) < 30:
-            return z
-    raise ValueError(f"Not close enough to any of the disks {true_z}")
-
-pixel_hits = {(s_bin, z_bin): [0]*100 for s_bin in range(87) for z_bin in range(192)}
+cells = {i + j: [0]*100 for i in range(1,81922,16384) for j in range(0,3585,256)}
+r = 13.7
 for i in range(100):
     print(f"starting event {i}")
     event = events[i]
-    hits = {coord: [] for coord in layer_radii + disk_z}
+    for hit in event.get("VTXIBCollection"):
+        if radius(hit) == 14:
+            cells[hit.getCellID()][i] += 1
 
-    for collection in ["VTXIBCollection", "VTXOBCollection", "VTXDCollection"]:
-        for hit in event.get(collection):
+pix_per_mod = 430080
+module_hit_averages = [np.mean(cells[mod]) for mod in cells.keys()]
+max_hits = max(module_hit_averages)
+avg_hits = np.mean(module_hit_averages)
 
-            if collection != "VTXDCollection":
-                hits[radius(hit)].append(hit)
-            else:
-                hits[z_coord(hit)].append(hit)
-
-    for hit in hits[14]:
-        r = true_radii[0]
-        azimuthal = phi(hit.getPosition().x, hit.getPosition().y)
-        s = r * azimuthal
-        z = hit.getPosition().z
-        if abs(z) > 96:
-            continue
-        s_pixel = int(s // 1)
-        z_pixel = int((z + 96) // 1)
-        pixel_hits[(s_pixel, z_pixel)][i] += 1
-
-pixel_hit_averages = {pix: np.mean(pixel_hits[pix]) for pix in pixel_hits.keys()}
-max_hits = max(pixel_hit_averages.values())
-avg_hits = np.mean(list(pixel_hit_averages.values()))
-
-hist = ROOT.TH2F("hits", "Guinea Pig Hits Per Area (mm^-2)", 192, -96, 96, 86, 0, 86)
-for s, z in pixel_hit_averages.keys():
-    hist.SetBinContent(z + 1, s + 1, pixel_hit_averages[(s, z)])
-
-hist.SetStats(0)
-hist.SetTitle("Guinea Pig Layer 1 Hits Per Area (mm^-2);z (mm);Arc Distance (mm)")
-canvas = ROOT.TCanvas("hits", "Guinea Pig Hits Per Area (mm^-2)")
-hist.Draw("colz")
-canvas.Update()
-canvas.SaveAs("../plots/hit_rates/per_area/occupancy.png")
-
-print(f"Maximum occupancy: {max_hits*25*25*15/1000000}")
-print(f"Average occupancy: {avg_hits*25*25*15/1000000}")
+print(f"Maximum occupancy: {max_hits*15/pix_per_mod}")
+print(f"Average occupancy: {avg_hits*15/pix_per_mod}")
