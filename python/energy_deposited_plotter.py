e6b3d0872934ac96aae0fd733c8a1748f2fbc728
natem297 <mnate5002@gmail.com>
2024-08-06 11:07:44 +0200

    updated energy deposited plots to dE/dx

--- python/energy_deposited_plotter.py
+++ python/energy_deposited_plotter.py
@@ -51,39 +51,30 @@ for event in events:
         for hit in event.get(collection):
             layers[radius(hit)].append(hit)
 
-no_edep_pdgs = {11: 0, -11: 0, 22: 0}
-good_cell_ids = set()
-num_no_edep_in_good_cells = 0
-
 ROOT.gStyle.SetPalette(ROOT.kRainBow)
 
 # creates histogram for each layer
 for layer_index in range(5):
     cos_map_hist = ROOT.TH2F("events", f"Guinea Pig Layer {layer_index + 1} Energy Deposited and Cosine Theta", 100, 0, 1, 100, 0, 100)
-    lepton_hist = ROOT.TH1F("lepton energy", f"Guinea Pig Layer {layer_index + 1} Lepton Energy Deposited", 50, 0, 50)
-    non_lepton_hist = ROOT.TH1F("non lepton energy", f"Guinea Pig Layer {layer_index + 1} Non Lepton Energy Deposited", 50, 0, 50)
+    hist = ROOT.TH1F("energy", f"Guinea Pig Layer {layer_index + 1} Energy Deposited", 70, 0, 700)
     phis, zs = [], []
-    cos_thetas = [[] for _ in range(100)]
 
-    for particle in layers[layer_radii[layer_index]]:
-        edep = 1000000*particle.getEDep()
-        mc = particle.getMCParticle()
-        pdg = mc.getPDG()
+    for hit in layers[layer_radii[layer_index]]:
+        edep = 1000000*hit.getEDep()
+        path_length = hit.getPathLength()
 
+        # if hit.isProducedBySecondary():
+        #     continue
         # tracks location of cells with very low energy deposited
         if edep < 2:
-            mc = particle.getMCParticle()
-            phis.append(phi(particle.getPosition().x, particle.getPosition().y))
-            zs.append(particle.getPosition().z)
+            phis.append(phi(hit.getPosition().x, hit.getPosition().y))
+            zs.append(hit.getPosition().z)
 
         # else:
-        if pdg == 11 or pdg == -11:
-            lepton_hist.Fill(edep)
-        else:
-            non_lepton_hist.Fill(edep)
-        cos_theta = math.cos(theta(particle.getPosition().x, particle.getPosition().y, particle.getPosition().z))
+        hist.Fill(edep/path_length)
+
+        cos_theta = math.cos(theta(hit.getPosition().x, hit.getPosition().y, hit.getPosition().z))
         cos_map_hist.Fill(cos_theta, edep)
-        cos_thetas[int(cos_theta//0.01)].append(edep)
 
     # cos_map_hist.SetTitle(f"Guinea Pig Layer {layer_index + 1} Energy Deposited and Cosine Theta;Cosine Theta;Energy (keV)")
     # cos_map_hist.SetStats(0)
@@ -92,35 +83,13 @@ for layer_index in range(5):
     # cos_map_canvas.Update()
     # cos_map_canvas.SaveAs(f"../plots/energy_deposited/regular/total_edep/layer{layer_index+1}_edep_and_cos.png")
 
-    # cos_hist = ROOT.TH1F("energy", f"Electron Gun Layer {layer_index + 1} Energy Deposited over Cosine Theta", 100, 0, 1)
-    # for j in range(100):
-    #     if not cos_thetas[j]:
-    #         cos_hist.SetBinContent(j + 1, 0)
-    #     else:
-    #         cos_hist.SetBinContent(j + 1, np.mean(cos_thetas[j]))
-    # cos_hist.GetXaxis().SetTitle("Cosine Theta")
-    # cos_hist.GetYaxis().SetTitle("Energy (keV)")
-    # cos_hist.SetStats(0)
-    # cos_canvas = ROOT.TCanvas("energy", f"Layer {layer_index + 1} Energy Deposited over Cosine Theta")
-    # cos_hist.Draw()
-    # cos_canvas.Update()
-    # cos_canvas.SaveAs(f"../plots/energy_deposited/regular/total_edep/edep_layer{layer_index + 1}_cos.png")
-
-    lepton_hist.SetStats(0)
-    lepton_hist.GetXaxis().SetTitle("Energy (keV)")
-    lepton_hist.GetYaxis().SetTitle("Number of Events")
-    canvas = ROOT.TCanvas("energy", f"Layer {layer_index + 1} Energy Deposited")
-    lepton_hist.Draw()
+    hist.SetStats(0)
+    hist.GetXaxis().SetTitle("dE/dx (keV/mm)")
+    hist.GetYaxis().SetTitle("Number of Events")
+    canvas = ROOT.TCanvas("energy", f"Guinea Pig Layer {layer_index + 1} dE/dx")
+    hist.Draw()
     canvas.Update()
-    canvas.SaveAs(f"../plots/energy_deposited/guinea_pig/total_edep/lepton_energy_deposited_layer{layer_index + 1}_eg.png")
-
-    non_lepton_hist.SetStats(0)
-    non_lepton_hist.GetXaxis().SetTitle("Energy (keV)")
-    non_lepton_hist.GetYaxis().SetTitle("Number of Events")
-    canvas = ROOT.TCanvas("energy", f"Layer {layer_index + 1} Energy Deposited")
-    non_lepton_hist.Draw()
-    canvas.Update()
-    canvas.SaveAs(f"../plots/energy_deposited/guinea_pig/total_edep/nonlepton_energy_deposited_layer{layer_index + 1}_eg.png")
+    canvas.SaveAs(f"../plots/energy_deposited/guinea_pig/total_edep/gp_edep_layer{layer_index + 1}_secondary.png")
 
     # hist = ROOT.TH2F("locations", "Electron Gun Locations of 0 Energy Deposited", 120, 0, 360, 150, -150, 150)
     # hist.SetTitle(f"Layer {layer_index + 1} Locations of Hits with 0 Energy Deposited;Phi (radians);Z (mm)")
@@ -133,21 +102,23 @@ for layer_index in range(5):
     # canvas.Update()
     # canvas.SaveAs(f"../plots/energy_deposited/electron_gun/edep_maps/layer{layer_index+1}_no_edep_locations_eg.png")
 
-# hist = ROOT.TH1F("energy", "Electron Gun Disks Energy Deposited", 50, 0, 50)
-# for event in events:
-#     for hit in event.get("VTXDCollection"):
-#         edep = 1000000*hit.getEDep()
-#         if edep < 0.000002:
-#             continue
-#         else:
-#             hist.Fill(edep)
+hist = ROOT.TH1F("energy", "Guinea Pig Disks dE/dx", 70, 0, 700)
+for event in events:
+    for hit in event.get("VTXDCollection"):
+        edep = 1000000*hit.getEDep()
+        path_length = hit.getPathLength()
 
-# hist.SetStats(0)
-# hist.GetXaxis().SetTitle("Energy (keV)")
-# hist.GetYaxis().SetTitle("Number of Events")
-# canvas = ROOT.TCanvas("energy", "Disk Energy Deposited")
-# hist.Draw()
-# canvas.Update()
-# canvas.SaveAs("../plots/energy_deposited/electron_gun/total_edep/energy_deposited_disks_zoom_eg.png")
+        # if hit.isProducedBySecondary():
+        #     continue
+        # if edep < 2:
+        #     continue
+        # else:
+        hist.Fill(edep/path_length)
 
-print(no_edep_pdgs)
+hist.SetStats(0)
+hist.GetXaxis().SetTitle("dE/dx (keV/mm)")
+hist.GetYaxis().SetTitle("Number of Events")
+canvas = ROOT.TCanvas("energy", "Disk Energy Deposited")
+hist.Draw()
+canvas.Update()
+canvas.SaveAs("../plots/energy_deposited/guinea_pig/total_edep/gp_edep_disks_secondary.png")
